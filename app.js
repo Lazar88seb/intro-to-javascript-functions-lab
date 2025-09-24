c223b739dae28bf33dfe159a23b9761114df3267
Lazar88seb <lazarmoracanin96@gmail.com>
2025-09-24 15:46:58 +0200

    lab

--- app.js
+++ app.js
@@ -112,8 +112,17 @@ Example: maxOfThree(17, 4, 9) should return 17.
 Complete the exercise in the space below:
 */
 
-const maxOfThree(){
-    return 
+function maxOfThree(a, b, c){
+if (a>b&&a>c){
+    return a
+}else if(b>a&&b>c){
+    return b
+}else if (c>a&&c>b){
+    return c
+
+}
+
+
 }
 
 
